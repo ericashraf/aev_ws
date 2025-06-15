f19d84ba1ce71ba907c310552cead82963560859
ericashraf <ericashraf0@gmail.com>
2025-06-15 21:43:35 +0300

    urdf file

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/eric/aev_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/eric/aev_ws/devel;/home/eric/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/eric/aev_ws/devel;/home/eric/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,7 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/eric/aev_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/eric/aev_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/eric/aev_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/eric/aev_ws/src:$ROS_PACKAGE_PATH"
